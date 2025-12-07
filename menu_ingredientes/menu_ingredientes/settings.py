d86c2425c015f15d3e67545f9c4b458ceb4c826e
ShuliDev <patrick.alexander.2003@hotmail.com>
2025-12-07 15:53:25 -0300

    Update settings.py

--- menu_ingredientes/menu_ingredientes/settings.py
+++ menu_ingredientes/menu_ingredientes/settings.py
@@ -30,6 +30,9 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['web-production-2d3fb.up.railway.app', '127.0.0.1', 'localhost']
 
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = ['https://web-production-2d3fb.up.railway.app']
+
 
 # Application definition
 
