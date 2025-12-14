fe4042c8fc3b7a1f714f6fa06b09accf15872067
Alan Veas <alan.veas02@inacapmail.cl>
2025-12-14 15:33:19 -0300

    Update serializers.py
    Actualizado

--- menu_ingredientes/mainApp/serializers.py
+++ menu_ingredientes/mainApp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Perfil, Mesa, Reserva
+from .models import Perfil, Mesa, Reserva, CategoriaMenu, Ingrediente, Plato, Receta, Stock, ReservaStock
 from datetime import date, time
 import re
 
